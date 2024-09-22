dd65d24cdf4f2e8d6ae17321f0095b4a563cda8d
f-h1 <154448936+f-h1@users.noreply.github.com>
2024-09-22 19:36:56 +0200

    update

--- lua/nvim-basic-settings/init.lua
+++ lua/nvim-basic-settings/init.lua
@@ -3,6 +3,15 @@ local M = {}
 
 function M.setup()
 	-- This function will be called automatically when the plugin is loaded
+	-- Get the current time
+	local current_time = os.date("%Y-%m-%d %H:%M:%S")
+
+	-- Print a message with the timestamp
+	print(string.format("[nvim-basic-settings] Plugin setup started at: %s", current_time))
+
+	-- Your setup code here
+	-- Example: Setting some configuration options
+	vim.o.number = true -- Enable line numbers
 end
 
 return M
